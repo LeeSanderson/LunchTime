302dc50d9e01d8caf5dd68e01eeac88fe13e1fff
LeeSanderson <lee.sanderson@codurance.com>
2025-08-29 17:03:56 +0100

    Manually remove duplicate RestaurantContext that Copilot refused to remove

--- LunchTimeMCP/RestaurantService .cs
+++ LunchTimeMCP/RestaurantService .cs
@@ -123,14 +123,3 @@ public class RestaurantService
         restaurants.AddRange(trendyRestaurants);
     }
 }
-
-[JsonSerializable(typeof(List<Restaurant>))]
-[JsonSerializable(typeof(Restaurant))]
-[JsonSerializable(typeof(RestaurantVisitInfo))]
-[JsonSerializable(typeof(Dictionary<string, RestaurantVisitInfo>))]
-[JsonSerializable(typeof(RestaurantData))]
-[JsonSerializable(typeof(FormattedRestaurantStat))]
-[JsonSerializable(typeof(FormattedRestaurantStats))]
-internal sealed partial class RestaurantContext : JsonSerializerContext
-{
-}
